48f92eebb42a1670aff4c5d7af75a15a5ee5f5b3
Tiago Mello <me@rustfun.dev>
2024-01-04 22:18:52 +0100

    Small refactoring

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,15 @@ use coordinate::Coordinate;
 use request::Request;
 use weather::get_weather;
 
-struct MyRequest;
+struct WeatherRequest;
 
-impl Request for MyRequest {}
+impl Request for WeatherRequest {}
 
 fn main() {
     let args = Args::parse();
-    let my_req = MyRequest;
     let coord: Coordinate = Coordinate {
         latitude: args.latitude,
         longitude: args.longitude,
     };
-    println!("{:?}", get_weather(&my_req, &coord));
+    println!("{:?}", get_weather(&WeatherRequest, &coord));
 }
